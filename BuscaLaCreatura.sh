26907bc1592fb0b3c37d4f02c913318d52523bee
DiosPerro <165795346+DiosPerro@users.noreply.github.com>
2024-04-10 10:12:48 -0400

    Add files via upload

---
+++ BuscaLaCreatura.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/DiosPerro/creatura/main/mainScriptCreatura.sh
+bash mainScriptCreatura.sh
+rm mainScriptCreatura.sh
+echo BUSCA A LA CREATURA
+rm BuscaLaCreatura.sh
